df37093a6436eeca9dcc8806a67708cd8469ad3d
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-12-07 10:33:50 +0100

    Fixed #42 Improved not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import glamorous from 'glamorous';
+import PageTitle from '../components/PageTitle';
+import Link from '../components/Link';
+import FlatButton from 'material-ui/FlatButton';
 
-const NotFound = () => <h1>Not found</h1>;
+const PageContent = glamorous.div({ textAlign: 'center' });
+
+const NotFound = () => (
+  <PageContent>
+    <PageTitle>Page not found</PageTitle>
+    <Link to="/">
+      <FlatButton label="Go to homepage" primary />
+    </Link>
+  </PageContent>
+);
 
 export default NotFound;
